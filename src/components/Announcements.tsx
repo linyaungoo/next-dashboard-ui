72729a1db53f4a55b3ffea06d5239ed2808cd54c
Lin Yaung Oo <linyaungoo80@gmail.com>
2024-10-25 15:55:25 +0630

    commit

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -2,7 +2,35 @@
 
 const Announcements = () => {
   return (
-    <div className=''>Announcements</div>
+    <div className='bg-white p-4 rounded-md'>
+        <div className="flex items-center justify-between">
+            <h1 className="text-xl font-semibold">Announcements</h1>
+            <span className="text-xs text-gray-500">View All</span>
+        </div>
+        <div className="flex flex-col gap-4">
+        <div className="bg-lamaSkyLight rounded-md p-4">
+                <div className="flex items-center justify-between p-2">
+                    <h2 className="font-medium">Larnow al ians iatal cnata</h2>
+                    <span className="text-xs text-gray-400 bg-white rounded-md p-1">2025-01-01</span>
+                </div>
+                <div>Tjatl at atjla tjatl aflj aa jtatl tatat eat ncnal tajt testl tat atalt atantn tnatn agnatnzz a</div>
+            </div>
+            <div className="bg-lamaPurpleLight rounded-md p-4">
+                <div className="flex items-center justify-between p-2">
+                    <h2 className="font-medium">Larnow al ians iatal cnata</h2>
+                    <span className="text-xs text-gray-400 bg-white rounded-md p-1">2025-01-01</span>
+                </div>
+                <div>Tjatl at atjla tjatl aflj aa jtatl tatat eat ncnal tajt testl tat atalt atantn tnatn agnatnzz a</div>
+            </div>
+            <div className="bg-lamaYelllowLight rounded-md p-4">
+                <div className="flex items-center justify-between p-2">
+                    <h2 className="font-medium">Larnow al ians iatal cnata</h2>
+                    <span className="text-xs text-gray-400 bg-white rounded-md p-1">2025-01-01</span>
+                </div>
+                <div>Tjatl at atjla tjatl aflj aa jtatl tatat eat ncnal tajt testl tat atalt atantn tnatn agnatnzz a</div>
+            </div>
+        </div>
+    </div>
   )
 }
 
